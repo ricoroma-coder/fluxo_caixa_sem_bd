e0ee2d326a0e9fc0cdbd1a684c7aa64e79212fd9
henrique-goncalves-cd2 <91959580+henrique-goncalves-cd2@users.noreply.github.com>
2022-06-08 15:38:46 -0300

    Importando bootstrap e jQuery, e montando header e parte da section em index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<html style="min-height: 100vh;min-width: 100vw;">
+    <head>
+        <title>Fluxo de Caixa</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <header class="row bg-dark m-0 p-2 w-100">
+            <div id="title" class="p-2 text-light font-weight-bold" style="font-size: 1.3rem;font-weight: bolder;">Fluxo de Caixa</div>
+        </header>
+
+        <section class="container">
+            <div id="head-section" class="d-flex justify-content-between p-1 mt-3">
+                <div>
+                    <button class="btn btn-success rounded" style="font-size: 1.1rem;">Criar</button>
+                </div>
+
+                <div>
+                    <button class="btn btn-danger rounded" style="font-size: 1.1rem;">Remover</button>
+                </div>
+            </div>
+
+            <hr/>
+
+            <div id="content-section" class="d-flex p-1 mt-1">
+                <div class="flex-1 p-1 w-50">
+                    
+                </div>
+
+                <div class="flex-1 p-1 w-50">
+                    
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
