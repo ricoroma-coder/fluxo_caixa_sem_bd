bde460bd5559b3e8c9dd818a7011299f8c9cd6fd
henrique-goncalves-cd2 <91959580+henrique-goncalves-cd2@users.noreply.github.com>
2022-06-08 19:19:21 -0300

    Finalizando operações básicas em index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<?php
+    require_once __DIR__ . "/php/index.php";
+?>
+
 <html style="min-height: 100vh;min-width: 100vw;">
     <head>
+        <meta charset="utf-8"/>
         <title>Fluxo de Caixa</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <script src="js/jquery.min.js"></script>
@@ -23,13 +28,88 @@
 
             <hr/>
 
-            <div id="content-section" class="d-flex p-1 mt-1">
-                <div class="flex-1 p-1 w-50">
-                    
+            <div id="content-section" class="d-flex p-2 mt-1">
+                <div class="flex-1 p-3 w-50">
+                    <table class="border boder-1 w-100">
+                        <thead>
+                            <th class="text-center p-2 border border-1 bg-dark text-light" scope="100%">Fluxos</th>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $dir = scandir("1-fluxos");
+                                if (!$dir || sizeof($dir) <= 2) {
+                            ?>
+                                <tr>
+                                    <td>Não há fluxos...</td>
+                                </tr>
+                            <?php
+                                }
+                                else {
+                                    $lista = organizarPastas($dir);
+                                    $ultimos = [];
+                                    foreach ($lista as $ano => $arquivos) {
+                            ?>
+                                <tr>
+                                    <td>
+                                        <div class="d-flex w-100">
+                                            <div class="border border-1">
+                                                <img src="imagens/ico_pasta.png" style="height: 40px;width: 40px;">
+                                            </div>
+                                            <div class="p-1 border border-1 w-100 d-flex align-items-center"><?php echo $ano; ?></div>
+                                        </div>
+
+                                        <?php
+                                            foreach ($arquivos as $arquivo) {
+                                                checarDataRecentes("{$ano}/{$arquivo}.html", $ultimos);
+                                        ?>
+                                            <tr>
+                                                <td>
+                                                    <div class="d-flex" style="margin-left: 40px; flex: 1;">
+                                                        <div class="p-1 border border-1">
+                                                            <img src="imagens/ico_arq.png" style="height: 30px;width: 30px;">
+                                                        </div>
+                                                        <div class="p-1 border border-1 w-100 d-flex align-items-center"><?php echo $arquivo; ?></div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        <?php
+                                            }
+                                        ?>
+                                    </td>
+                                </tr>
+                            <?php
+                                    }
+                                }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
 
-                <div class="flex-1 p-1 w-50">
-                    
+                <div class="flex-1 p-3 w-50">
+                    <table class="border boder-1 w-100">
+                        <thead>
+                            <th class="text-center p-2 border border-1 bg-dark text-light" scope="2">Recentes</th>
+                        </thead>
+                        <tbody>
+                            <?php
+                                foreach (ordenarRecentes($ultimos) as $arquivo) {
+                            ?>
+                                <tr>
+                                    <td>
+                                        <div class="d-flex w-100 position-relative">
+                                            <div class="border border-1">
+                                                <img src="imagens/ico_arq.png" style="height: 50px;width: 50px;">
+                                            </div>
+                                            <div class="p-1 border border-1 w-100 d-flex align-items-center"><?php echo $arquivo['nome']; ?></div>
+                                            <div class="position-absolute" style="bottom: 0;right: 0;font-size: 14px;"><?php echo date("d/m/Y H:i:s", substr($arquivo['data'], 0, 10)); ?></div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php
+                                }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </section>
---
+++ php/index.php
@@ -0,0 +1,48 @@
+<?php
+
+function limparArray($array) {
+    unset($array[0]);
+    unset($array[1]);
+
+    return $array;
+}
+
+function organizarPastas($fluxos) {
+    $fluxos = limparArray($fluxos);
+    $array = [];
+
+    foreach ($fluxos as $ano) {
+        $arquivos = limparArray(scandir("1-fluxos/{$ano}"));
+        $array[$ano] = [];
+
+        foreach ($arquivos as $arquivo) {
+            if (substr($arquivo, -5) == ".html")
+                $array[$ano][] = str_replace(".html", "", $arquivo);
+        }
+    }
+
+    return $array;
+}
+
+function checarDataRecentes($caminho_arq, &$ultimos) {
+    $ultimos[] = ["nome" => $caminho_arq, "data" => filemtime("1-fluxos/{$caminho_arq}")];
+}
+
+function ordenarRecentes($recentes) {
+    $auxiliar = [];
+
+    foreach ($recentes as $chave => $arquivo) {
+        $auxiliar[$chave] = $arquivo['data'];
+    }
+
+    $copia_auxiliar = $auxiliar;
+    rsort($copia_auxiliar);
+    $retorno = [];
+
+    foreach ($copia_auxiliar as $data) {
+        $chave = array_search($data, $auxiliar);
+        $retorno[] = $recentes[$chave];
+    }
+
+    return $retorno;
+}
